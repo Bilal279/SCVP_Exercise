aa5cd538ccd0e7739bde97d190fcfcfa6cd40ea8
muhammad Bilal <111465530+Bilal279@users.noreply.github.com>
2024-03-19 11:52:35 +0100

    Update FIR.h

--- FIR.h
+++ FIR.h
@@ -1,36 +1,12 @@
-SC_MODULE(FIR)
+class memory : sc_module, tlm::tlm_fw_transport_if<>
 {
-    sc_in<bool> in;
-    sc_signal<bool> r1;
-    sc_signal<bool> r2;
-    sc_signal<bool> r3;
-    sc_out<bool> out;
-    sc_clk<bool> Clk;
-    sc_in<bool> reset
+    private:
+    unsigned char mem[memory_size];
 
-    SC_CTOR(FIR) : in("in"), Clk("Clk"), out("out"),reset("reset"),r1("r1"),r2("r2"),r3("r3")
-    {
-        SC_METHOD(conbinational);
-        sensitive << r1 << r2 << r3;
-        SC_METHOD(sequential);
-        sensitive << Clk.pos();
-    }
+    public:
+    tlm::tlm_target_socket<> tSocket;
 
-    void combinational()
+    SC_CTOR(memory) : tSocket("tSocket")
     {
-        r3 = r2;
-        r2 = r1;
-        r1 = in;
+        tSocket.bind(*this);
     }
-    void sequential()
-    {
-        if(reset == 1)
-        {
-            r1, r2, r3 = 0;
-        }
-        else
-        {
-            out = (in + r1 + r2 + r3)/4;
-        }
-    }
-};
